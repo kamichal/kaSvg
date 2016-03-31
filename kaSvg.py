cb2055bc80bc475484825f9ce337d92501a0c196
kamichal <bucowski@gmail.com>
2016-03-31 18:01:09 +0000

    Nothing changed :D

--- kaSvg.py
+++ kaSvg.py
@@ -2,12 +2,13 @@
 '''
 Created on 30 Dec 2015
 
-@author: kamichal, basing on aroberge work 
+@author: kamichal, basing on aroberge work
 
 This module contains all the classes required to easily create an xml
 document containing svg diagrams.
 
 '''
+
 
 class XmlElement(object):
     '''Prototype from which all the xml elements are derived.
@@ -25,7 +26,7 @@ class XmlElement(object):
             for attKey in attributes.keys():
                 fixattr = attKey.replace('_', '-').lower()
                 self.attributes[fixattr] = attributes[attKey]
-                '''lower case is needed for converting 'Class' 
+                '''lower case is needed for converting 'Class'
                 (class is forbiden in such use)
                 you pass stroke_width=0.2 and it's being converted to
                 stroke-widt=0.2'''
@@ -36,8 +37,8 @@ class XmlElement(object):
         if attributes is not None:
             for attKey in attributes.keys():
                 fixattr = attKey.replace('_', '-').lower()
-                self.attributes[fixattr] = attributes[attKey]        
-                
+                self.attributes[fixattr] = attributes[attKey]
+
     def __repr__(self):
         return self.indRepr(0)
 
@@ -70,21 +71,23 @@ class XmlElement(object):
 
         return ''.join(render)
 
+
 def indentStr(level):
     ''' Global function that returns indentation string'''
     return level * "  "
+
 
 class SvgWindow(XmlElement):
     def __init__(self, width, height, **attributes):
         attributes["width"] = width
         attributes["height"] = height
         XmlElement.__init__(self, "svg", **attributes)
-        self.attributes["xmlns"] = "http://www.w3.org/2000/svg" 
+        self.attributes["xmlns"] = "http://www.w3.org/2000/svg"
         self.attributes["xmlns:xlink"] = "http://www.w3.org/1999/xlink"
-    
+
     def __repr__(self):
         return self.indRepr(0)
-      
+
     def useElement(self, xlinkId, x, y, **attributes):
         extraTransform = ""
         for att in attributes:
@@ -98,60 +101,62 @@ class SvgWindow(XmlElement):
     def store(self, thatfilename):
         with open(thatfilename, 'w') as ff:
             ff.write(str(self))
-        ff.close()    
+        ff.close()
 
 
 class SvgStyleDefinitionEntry(object):
     def __init__(self, name, **attrList):
         self.name = name
-        self.attributes = {} 
+        self.attributes = {}
         if attrList is not None:
             for attKey in attrList.keys():
                 fixattr = attKey.replace('_', '-').lower()
-                '''you pass stroke_width=0.2 and it needs to be 
+                '''you pass stroke_width=0.2 and it needs to be
                 converted to stroke-widt=0.2'''
                 self.attributes[fixattr] = attrList[attKey]
-            
+
     def indRepr(self, indentLevel=0):
         render = ["%s%s {\n" % (indentStr(indentLevel), self.name)]
         for att in self.attributes:
             render.append("%s%s: %s;\n" % (indentStr(indentLevel + 1), att, self.attributes[att]))
         render.append("%s}\n" % (indentStr(indentLevel)))
         return ''.join(render)
-    
+
     def append(self, **attrList):
         '''not sure if it's ok'''
         # self.attributes = {self.attributes, attrList}
         self.attributes = dict(zip(self.attributes, attrList))
 
+
 class SvgStylesContainer(XmlElement):
     def __init__(self):
         self.styles = []
-    
+
     def append(self, _SvgStyleDefinitionEntry):
         self.styles.append(_SvgStyleDefinitionEntry)
-    
+
     def indRepr(self, indentLevel=0):
         if self.styles:
             render = ["%s<style type=\"text/css\">\n" % (indentStr(indentLevel))]
             render.append("%s<![CDATA[\n" % (indentStr(indentLevel + 1)))
-            for styleClass in self.styles:        
+            for styleClass in self.styles:
                 render.append(styleClass.indRepr(indentLevel + 2))
             render.append("%s]]>\n" % (indentStr(indentLevel + 1)))
             render.append("%s</style>\n" % (indentStr(indentLevel)))
             return ''.join(render)
         else:
             return ''
-        
+
+
 class SvgDefinitionsContainer(XmlElement):
     '''Short-cut to create svg defs.  A user creates an instance of this
     object and simply appends other svg Elements'''
     def __init__(self):
-#         self.root = XmlElement("svg", width=0, height=0)
+        # self.root = XmlElement("svg", width=0, height=0)
         self.root = XmlElement("defs")
         self.styles = SvgStylesContainer()
         self.root.append(self.styles)
-        
+
     def append(self, other):
         '''appends other to defs sub-element, instead of root element'''
         self.root.append(other)
@@ -163,18 +168,22 @@ class SvgDefinitionsContainer(XmlElement):
     def indRepr(self, indentLevel):
         return "\n" + self.root.indRepr(indentLevel) + "\n"
 
+
 class DefineSvgGroup(XmlElement):
     def __init__(self, defId, SvgDefContainer, **attributes):
         attributes["id"] = defId
         XmlElement.__init__(self, "g", **attributes)
         SvgDefContainer.append(self)
-                    
+
+
 class XmlComment(object):
     '''Comment that can be inserted in code xml documents'''
     def __init__(self, text):
         self.text = text
+
     def __repr__(self):
         return "<!-- [ " + self.text + " ] -->\n"
+
     def indRepr(self, indentLevel):
         return "%s%s" % (indentStr(indentLevel), self)
-    
+
